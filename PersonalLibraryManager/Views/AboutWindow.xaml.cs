a2d89372dc27e5992bb79f4de6ff8f1304a370f4
Maximilian <prezmaximilian@gmail.com>
2026-01-28 10:32:50 +0300

    Redesigned the visuals (GUI) of all the Windows

--- PersonalLibraryManager/Views/AboutWindow.xaml.cs
+++ PersonalLibraryManager/Views/AboutWindow.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace PersonalLibraryManager.Views
+﻿using System.Windows;
+
+namespace PersonalLibraryManager.Views
 {
     public partial class AboutWindow : System.Windows.Window
     {
         public AboutWindow()
         {
             InitializeComponent();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
